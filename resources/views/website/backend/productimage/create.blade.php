719cedc0a6866ce0a423eb36595550c055828d6a
Nikesh Adhikari <nikeshadhikari2017@gmail.com>
2020-06-06 14:45:21 +0545

    just cleaned some comments

--- resources/views/website/backend/productimage/create.blade.php
+++ resources/views/website/backend/productimage/create.blade.php
@@ -10,10 +10,7 @@
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
             <ul class="dropdown-menu" role="menu">
-              {{-- <li><a class="dropdown-item" href="#">Settings 1</a>
-              </li>
-              <li><a class="dropdown-item" href="#">Settings 2</a>
-              </li> --}}
+
             </ul>
           </li>
           <li><a class="close-link"><i class="fa fa-close"></i></a>
@@ -42,39 +39,6 @@
           </div>
 
 
-          {{-- <div class="item form-group">
-            <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Last Name <span class="required">*</span>
-            </label>
-            <div class="col-md-6 col-sm-6 ">
-              <input type="text" id="last-name" name="last-name" required="required" class="form-control">
-            </div>
-          </div>
-          <div class="item form-group">
-            <label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Middle Name / Initial</label>
-            <div class="col-md-6 col-sm-6 ">
-              <input id="middle-name" class="form-control" type="text" name="middle-name">
-            </div>
-          </div>
-          <div class="item form-group">
-            <label class="col-form-label col-md-3 col-sm-3 label-align">Gender</label>
-            <div class="col-md-6 col-sm-6 ">
-              <div id="gender" class="btn-group" data-toggle="buttons">
-                <label class="btn btn-secondary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-                  <input type="radio" name="gender" value="male" class="join-btn" data-parsley-multiple="gender"> &nbsp; Male &nbsp;
-                </label>
-                <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-                  <input type="radio" name="gender" value="female" class="join-btn" data-parsley-multiple="gender"> Female
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="item form-group">
-            <label class="col-form-label col-md-3 col-sm-3 label-align">Date Of Birth <span class="required">*</span>
-            </label>
-            <div class="col-md-6 col-sm-6 ">
-              <input id="birthday" class="date-picker form-control" required="required" type="text">
-            </div>
-          </div> --}}
           <div></div>
           <div class="ln_solid"></div>
           <div class="item form-group">
